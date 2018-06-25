83ebcdf64d683f0ed7bfdeeb9814bb1c2f9ba6e3
rossmcarthur <r.mcarth20@gmail.com>
2018-06-24 21:11:27 -0400

    Working month incrementer/decrementer

--- frontend/components/calendar_index.jsx
+++ frontend/components/calendar_index.jsx
@@ -28,6 +28,7 @@ class CalendarIndex extends React.Component {
       currentMonth: null,
       months: MONTHS
     };
+    this.handleMonthChange = this.handleMonthChange.bind(this);
   }
 
   componentDidMount() {
@@ -37,6 +38,12 @@ class CalendarIndex extends React.Component {
     });
   }
 
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (prevState.currentMonth !== this.state.currentMonth) {
+  //
+  //   }
+  // }
+
   getDays() {
     const month = this.state.currentMonth;
     const weeks = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
@@ -44,24 +51,54 @@ class CalendarIndex extends React.Component {
     const firstDate = new Date(firstDay).getDay();
     firstDay = weeks[firstDate];
 
+    const fillerDays = _.range(0, firstDate + 1).map( (day, i) => {
+      return (
+        <DateItem day='none' date={null} key={day + 100}/>
+      );
+    });
+
     const days = _.range(1, MONTHS[month] + 1).map( (day, i) => {
     let date = new Date(this.state.currentDate.setDate(day));
       return (
         <DateItem day={day} key={i} date={date}/>
       );
     });
-    return days;
+    return { fillerDays, days };
+  }
+
+  handleMonthChange(move) {
+    let currentDate = this.state.currentDate;
+    if (move === "forward") {
+      currentDate = currentDate.setMonth(currentDate.getMonth() + 1, 1);
+      currentDate = new Date(currentDate);
+      this.setState({
+        currentDate,
+        currentMonth: Object.keys(this.state.months)[currentDate.getMonth()]
+      });
+    } else {
+      currentDate = currentDate.setMonth(currentDate.getMonth() - 1, 1);
+      currentDate = new Date(currentDate);
+      this.setState({
+        currentDate,
+        currentMonth: Object.keys(this.state.months)[currentDate.getMonth()]
+      });
+    }
   }
 
   render() {
+    const { days } = this.getDays();
+    const { fillerDays } = this.getDays();
     return (
       <div>
+        <button onClick={() => this.handleMonthChange('back')}>-</button>
+        <h1>{this.state.currentMonth}</h1>
+        <button onClick={() => this.handleMonthChange('forward')}>+</button>
         <WeekLabel />
-        {this.getDays()}
+        {fillerDays}
+        {days}
       </div>
     );
   }
-
 }
 
 const mapStateToProps = state => {
