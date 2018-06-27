2b88ce9fa1f58c6871e2655037428b6581d18bc1
rossmcarthur <r.mcarth20@gmail.com>
2018-06-27 16:13:09 -0400

    Update event model to handle overlapping events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   validates :description, :start_time, :end_time, :description, presence: true
-  validate :does_not_start_before_end
+  validate :does_not_start_before_end, :does_not_overlap_approved_request
 
   def start_before_end
   Event
@@ -11,6 +11,19 @@ class Event < ApplicationRecord
 def does_not_start_before_end
   unless start_before_end.empty?
     errors[:base] << 'Event cannot have a start time earlier than end time'
+  end
+end
+
+def overlapping_events
+  Event
+    .where.not(id: self.id)
+    .where.not('start_time > :end_time OR end_time < :start_time',
+                start_time: self.start_time, end_time: self.end_time)
+end
+
+def does_not_overlap_approved_request
+  unless overlapping_events.empty?
+    errors[:base] << 'Event overlaps with existing event'
   end
 end
 
